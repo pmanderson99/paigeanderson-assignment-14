6f9e8650a32957e5572df4e214feb030bc577e19
Paige Anderson <paige.m.anderson1999@gmail.com>
2023-10-07 13:35:19 -0700

    inital commit

---
+++ paigeanderson-assignment-14/src/main/java/com/coderscampus/assignment14/PaigeandersonAssignment14Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assignment14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaigeandersonAssignment14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaigeandersonAssignment14Application.class, args);
+	}
+
+}
---
+++ paigeanderson-assignment-14/src/test/java/com/coderscampus/assignment14/PaigeandersonAssignment14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assignment14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaigeandersonAssignment14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
