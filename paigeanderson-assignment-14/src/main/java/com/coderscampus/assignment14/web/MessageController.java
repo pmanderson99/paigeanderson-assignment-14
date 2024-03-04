3ffdf9f9465b3c15a8e25185dcf35ea67f427c71
Paige Anderson <paige.m.anderson1999@gmail.com>
2024-03-03 23:22:54 -0800

    v3.3

--- paigeanderson-assignment-14/src/main/java/com/coderscampus/assignment14/web/MessageController.java
+++ paigeanderson-assignment-14/src/main/java/com/coderscampus/assignment14/web/MessageController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +18,8 @@ public class MessageController {
 	@Autowired
 	private MessageService messageService;
 	
-	@PostMapping("/channels/{channelId}/messages")
+	@GetMapping("/channels/{channelId}/messages")
+	@ResponseBody
 	public List<Message> getMessages() {
 		return messageService.findAll();
 	}
--- paigeanderson-assignment-14/src/main/resources/static/js/channel.js
+++ paigeanderson-assignment-14/src/main/resources/static/js/channel.js
@@ -6,45 +6,46 @@ const username = sessionStorage.getItem('username');
 
 //Function to handle sending a message
 function sendMessage() {
-  const message = {
-	  "messageInput": messageInput.value.trim(),
-	  "channelId": channelId,
-	  "userName": username
-	  }
-  messageInput.value = ''
-  console.log('sending msg')
-  fetch(`/channels/{channelId}/createMessage`, {
-	  method: "POST",
-	  headers: {
-		  'Content-Type': 'application/json'
-	  },
-	  body: JSON.stringify(message)
-  }).then(response => {
-	  console.log(response)
-  })
-  if (message !== '') {
-    const chatMessages = document.querySelector('.chat-messages');
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('message');
-    messageElement.textContent = username +': ' + message;
-    chatMessages.appendChild(messageElement);
-    messageInput.value = '';
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-  }
+	const content = messageInput.value.trim()
+	const message = {
+		"messageInput": messageInput.value,
+		"channelId": channelId,
+		"userName": username
+	}
+	messageInput.value = ''
+	console.log('sending msg')
+	fetch(`/channels/{channelId}/createMessage`, {
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(message)
+	}).then(response => {
+		console.log(response)
+	})
+	if (content !== '') {
+		const chatMessages = document.querySelector('.chat-messages');
+		const messageElement = document.createElement('div');
+		messageElement.classList.add('message');
+		messageElement.textContent = username + ': ' + content;
+		chatMessages.appendChild(messageElement);
+		messageInput.value = '';
+		chatMessages.scrollTop = chatMessages.scrollHeight;
+	}
 }
 
 // Event listener
 document.getElementById('send-button').addEventListener('click', sendMessage);
 document.getElementById('message-input').addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-    sendMessage();
-  }
+	if (event.key === 'Enter') {
+		event.preventDefault();
+		sendMessage();
+	}
 });
 
 function extractChannelIdFromUrl() {
-    var currentUrl = window.location.href;
-    var segments = currentUrl.split('/');
-    let channelId = segments[segments.length - 1];
-    return channelId
-    }
+	var currentUrl = window.location.href;
+	var segments = currentUrl.split('/');
+	let channelId = segments[segments.length - 1];
+	return channelId
+}
