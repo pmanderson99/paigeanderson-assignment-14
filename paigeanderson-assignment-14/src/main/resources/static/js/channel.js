37ce8576910ef3fef630d4875c4ee3b28fe9213c
Paige Anderson <paige.m.anderson1999@gmail.com>
2025-01-20 18:23:15 -0800

    v1.20.25

--- paigeanderson-assignment-14/src/main/resources/static/js/channel.js
+++ paigeanderson-assignment-14/src/main/resources/static/js/channel.js
@@ -6,3 +6,65 @@ const user = sessionStorage.getItem('username');
 
 console.log(user, channelId);
 
+document.addEventListener('DOMContentLoaded', () => {
+	getMessages();
+	const msgsFromLocalStorage = JSON.parse(localStorage.getItem('messages')) || [];
+	
+	if(msgsFromLocalStorage > 0) {
+		createMessageElement(msgsFromLocalStorage);
+	}
+});
+
+
+function sendMessage() {
+	const message = {
+		userName: user.username,
+		messageText: messageInput.value,
+		channelId: channelId,
+		userId: user.userId
+	}
+	messageInput.value = '';
+	console.log('sending msg')
+	
+	fetch(`/channels/${channelId}/createMessage`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/JSON'
+		},
+		body: JSON.stringify(message)
+	}).then(response =>
+			response.json())
+			.then(data =>{
+				console.log(data)
+			})//.then( () => getMessages());
+	chatMessages.innerHTML += createMessageElement(message)	
+	messageInput.focus()	
+}
+
+function getMessages() {
+	const sentMessages = JSON.parse(localStorage.getItem('messages')) || [];
+	fetch(`/channels/${channelId}/getMessages`)
+		.then(response => response.json())
+		.then((messages) => {
+			const newMessages = messages.filter((message) => {
+				return message.channelId == channelId;
+			});
+			
+			if (newMessages.length > sentMessages?.length) { }
+			createMessageElement(newMessages);
+			
+			localStorage.setItem('messages', JSON.stringify(newMessages));
+		})
+}
+
+const createMessageElement = (message) => `
+	<div class="message"> <b>${message.userName}</b>: ${message.messageText}</div>
+`
+
+document.getElementById('send-button').addEventListener('click', sendMessage);
+document.getElementById('message-input').addEventListener('keydown', (event) => {
+	if (event.key === 'Enter') {
+		event.preventDefault();
+		sendMessage();
+	}
+});
